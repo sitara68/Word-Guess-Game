98c9f056542a641ccb9f2e864252a6bf5ed161fc
sitara68 <melaniecrose68@gmail.com>
2018-05-10 00:27:45 -0500

    Pseudocode html and javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+/*
+Create an array var with list of words with an Alice in Wonderland theme
+
+Create a function that chooses a random word from the Alice in Wonderland theme array
+
+Create a var for the number of Wins
+
+Create a var for the number of losses
+
+Create a var for the number of guesses remaining
+
+Create an array var for the Letters already guessed but do not populate it
+
+Initialize the fields in the Jumbotron with variable values for wins, losses and number of guesses remaining
+
+Initialize the letters already guessed
+
+Create an Event on a key stroke (up or down?)
+
+Create a function that parses the random word into an array with a letter in each index
+
+Create a space in the Jumbotron for each letter in the random word array
+
+Create a condition
+    -If the letter typed matches one of the letters in the array for the random word chosen then populate that letter in the space provided
+    -Else populate the letter in the Letters already guessed and reduce the number of guesses remaining by 1
+If the Number of Guesses = 0 then the user Losses and the Number of Losses increases by 1
+If the all the letters in the random word array are found, then the user Wins and the Number of Wins increases by 1
+
+Function to reset Letters already guessed, choose a new random word and reset the Blanks for the word
+
+*/
